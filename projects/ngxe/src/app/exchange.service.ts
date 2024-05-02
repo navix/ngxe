124d35507ee2b0c149bb7bd25ee82319a6d2eb07
Oleksa Novyk <gh@oleksanovyk.com>
2024-05-02 16:52:59 +0300

    fix exchange save url format

--- projects/ngxe/src/app/exchange.service.ts
+++ projects/ngxe/src/app/exchange.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Api_Error, Api_GetProject } from '../../../meta/api';
+import {formatExchangeFile} from '../../../meta/format-exchange-file';
 import {environment} from '../environments/environment';
 
 @Injectable({
@@ -14,10 +15,10 @@ export class ExchangeService {
   }
 
   loadProject({project, branch}: {project: string; branch: string}): Observable<Api_GetProject | Api_Error> {
-    return this.http.post<Api_GetProject | Api_Error>(`${environment.exchangeUrl}/api/project/load/${project}/${branch}`, {});
+    return this.http.post<Api_GetProject | Api_Error>(`${environment.exchangeUrl}/api/project/load/${formatExchangeFile(project)}/${formatExchangeFile(branch)}`, {});
   }
 
   saveProject({project, branch, body}: {project: string; branch: string, body: any}): Observable<true | Api_Error> {
-    return this.http.post<true | Api_Error>(`${environment.exchangeUrl}/api/project/save/${project}/${branch}`, body);
+    return this.http.post<true | Api_Error>(`${environment.exchangeUrl}/api/project/save/${formatExchangeFile(project)}/${formatExchangeFile(branch)}`, body);
   }
 }
